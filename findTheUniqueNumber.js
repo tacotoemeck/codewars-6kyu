f5e9779cfb027d2ed93f3f750c4741fac615c019
tacotoemeck <53991292+tacotoemeck@users.noreply.github.com>
2019-09-11 22:17:41 +0100

    Add files via upload

---
+++ findTheUniqueNumber.js
@@ -0,0 +1,26 @@
+/*
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains more than 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+*/
+
+function findUniq(arr) {
+  setVal = arr[0]
+  
+  return Number(arr.filter(val => val !== setVal))
+
+  if (isNaN(result)) {
+  	return 0
+  } else {
+  	return result
+  }
+  
+  
+}
+
+findUniq([ 0, 1, 0 ])
