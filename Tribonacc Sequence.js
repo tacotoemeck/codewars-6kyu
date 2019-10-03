5018f33be0b1de2c824c28d2fa6fed3a449decb0
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2019-10-03 17:42:13 +0100

    Add files via upload

---
+++ Tribonacc Sequence.js
@@ -0,0 +1,87 @@
+// Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+// As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+// So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+// [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+// But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+// [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+// Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+// Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+// If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
+
+// *[Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]*
+
+
+
+
+// below works but doesn't pass multiple tests as variables are declared outside of the function
+
+let result = [];
+let recCount = 0;
+
+function tribonacci(signature, num) {
+
+	console.log(signature[0])
+
+	let num1 = signature[0];
+	let num2 = signature[1];
+	let num3 = signature[2];
+	
+	if ( recCount == 0)  result.push(num1, num2, num3) 
+    
+    if ( num == 3) return result
+   
+	
+	let nextNum = num1 + num2 + num3
+
+	result.push(nextNum)
+	num1 = num2
+	num2 = num3
+	num3 = nextNum
+	recCount++
+
+    tribonacci(num1, num2, num3, num-1)
+	
+	return result
+}
+
+tribonacci([0,0,1],10)
+
+
+// below works
+
+function tribonacci(signature, num) {
+if ( num < 3 && num != 0) return signature.splice(0, num)
+if ( num == 0 ) return []
+var result = signature;
+let recCount = 0;
+
+let recursion = function(arr, n) {
+
+	if ( n == 3) {
+		return result;
+		
+	} else {
+		let nextNum = arr[0] + arr[1] + arr[2]
+		result.push(nextNum)
+		
+		let num1 = arr[1];
+		let num2 = arr[2];
+		let num3 = nextNum
+		
+		return recursion( [ num1, num2, num3], n-1)
+	}
+	 
+	}
+
+recursion(result, num)	
+
+return result
+}
+
+tribonacci([300,200,100],0)
