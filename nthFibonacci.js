875618dcf72c6cfa553f8523ff4b53e917548d19
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2020-02-22 17:40:56 +0000

    Add files via upload

---
+++ nthFibonacci.js
@@ -0,0 +1,24 @@
+// I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+// I would like for you to write me a function that when given a number (n) returns the n-th number in the Fibonacci Sequence.
+
+// For example:
+
+//    nthFibo(4) == 2
+// Because 2 is the 4th number in the Fibonacci Sequence.
+
+// For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+
+function nthFibo(n) {
+let seq = [0,1]
+let i = 0;
+    function generateFib(){
+        if (i + 1 == n ) return;
+        let next = seq[i] + seq[i+1];
+        seq.push(next);
+        i++;
+        generateFib()
+    }
+    generateFib()
+    return seq[i]
+  }
